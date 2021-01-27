e162eeae0396bde92839820e0f0959e9a112e5da
Artoiss <aarttu@windowslive.com>
2021-01-27 16:05:20 +0200

    Projects view with simple picture

--- homepage/views.py
+++ homepage/views.py
@@ -5,3 +5,6 @@ from django.http import HttpResponse
 
 def index(request):
     return render(request, 'homepage/index.html')
+
+def projects(request):
+    return render(request, 'homepage/projects.html')
